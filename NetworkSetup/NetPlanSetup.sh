770d3249d39fe316d9b2a17db2f41cabe155f3e6
kevingomez2017@gmail.com <kevingomez2017@gmail.com>
2022-04-16 01:24:46 +0000

    Finished netplan bash script for quick wifi setupwq

--- NetworkSetup/NetPlanSetup.sh
+++ NetworkSetup/NetPlanSetup.sh
@@ -6,9 +6,12 @@ interface=$3;
 
 if [! -f /etc/netplan/50-cloud-init.yaml ]; then
     if [-f ./50-cloud-init.yaml]; then
-        sed -i "s/ssid/$ssid/g"
-        sed -i "s/iface/$interface/g"
-        sed -i "s/pass/$pass/g"        
+        cp ./50-cloud-init.yaml ./50-cloud-init.yaml.bk
+        sed -i "s/ssid/$ssid/g" ./50-cloud-init.yaml
+        sed -i "s/iface/$interface/g" ./50-cloud-init.yaml
+        sed -i "s/pword/$pass/g" ./50-cloud-init.yaml
+        rm ./50-cloud-init.yaml
+        mv ./50-cloud-init.yaml.bk ./50-cloud-init.yaml
     else
         echo "Canot complete this command"
         exit 1
